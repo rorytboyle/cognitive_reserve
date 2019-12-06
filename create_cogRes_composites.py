548c94da63652c5d0383d357f55d99369e3b704e
rorytboyle <rorytboyle@gmail.com>
2019-12-06 13:19:46 +0000

    Update create_cogRes_composites.py
    Changed method for making composites - now can be extended to any number of variables
    Changed method for calculating composites - now calculates the mean across variables rather than the sum of variables

--- create_cogRes_composites.py
+++ create_cogRes_composites.py
@@ -1,58 +1,40 @@
 import pandas as pd
-import itertools
-import math as math
+from itertools import combinations
 
-
-def create_cogRes_composites(data, columns):
+def create_unique_combinations(df, columns):
     """
-    Standardises each cognitive reserve proxy score (i.e. z-scores) and
-    then creates a composite measure for every possible combination of
-    cognitive reserve proxies.
+    Creates a composite measure for every possible combination of
+    cognitive reserve proxies. Credit to StackOverflow User WeNYoBenfor optimising my code and giving 
+    a better solution https://stackoverflow.com/a/58895156/12384217
 
-    :param data: pandas dataframe containing subject ids (as the index) and
+    :param df: pandas dataframe containing subject ids (as the index) and
                 cognitive reserve data
     :param columns: List of strings containing column names for columns in data
                     containing cognitive reserve proxies.
-    :return cogResComposites: dataframe with....
+    :return composites: dataframe with every possible unique combination of 'columns' in 'data'.
     """
-    # select relevant cols
-    data = data[columns]
+    # Create unique combinations of each column, without repetition, based on column labels
+    unique_combos = sum([list(map(list, combinations(df.columns, i)))
+            for i in range(len(df.columns) + 1)], [])
     
-    # standardise each proxy
-    data = data.apply(lambda x: (x - x.mean()) / x.std(ddof=0))
+    # Sum the columns in each unique combination and add to a new dataframe
+    composites= pd.DataFrame(
+            {'_'.join(x) : df[x].mean(axis=1) for x in unique_combos if x !=[]})
+  
 
-    # Create composite scores - FIGURE OUT HOW TO ABSTRACT THIS FOR VARYING
-    #                           LENGTH/AMOUNT OF PROXIES
-    combos_2 = pd.DataFrame({'{}_{}'.format(a, b):
-        data[a] + data[b] 
-        for a, b in itertools.combinations(data.columns, 2)})
-        
-    combos_3 = pd.DataFrame({'{}_{}_{}'.format(a, b, c):
-        data[a] + data[b] + data[c] 
-        for a, b, c in itertools.combinations(data.columns, 3)})
-        
-    combos_4 = pd.DataFrame({'{}_{}_{}_{}'.format(a, b, c, d):
-        data[a] + data[b] + data[c] + data[d] 
-        for a, b, c, d
-        in itertools.combinations(data.columns, 4)})
-        
-    combos_5 = pd.DataFrame({'{}_{}_{}_{}_{}'.format(a, b, c, d, e):
-        data[a] + data[b] + data[c] + data[d] + data[e]
-        for a, b, c, d, e in itertools.combinations(data.columns, 5)})
-        
-    combos_6 = pd.DataFrame({'{}_{}_{}_{}_{}_{}'.format(a, b, c, d, e, f):
-        data[a] + data[b] + data[c] + data[d] + data[e] + data[f]
-        for a, b, c, d, e, f in itertools.combinations(data.columns, 6)})
-        
-    combos_7 = pd.DataFrame({'{}_{}_{}_{}_{}_{}_{}'.format(a, b, c, d, e, f, g):
-        data[a] + data[b] + data[c] + data[d] + data[e] + data[f] + data[g]
-        for a, b, c, d, e, f, g in itertools.combinations(data.columns, 7)})
-   
-    cogRes_composites = pd.concat([data, combos_2, combos_3, combos_4, combos_5,
-                                   combos_6, combos_7], axis=1)    
+def test_unique_combinations(df, columns, composites):
+    """
+    Runs some basic tests to check composites were created as intended.
 
+    :param df: pandas dataframe containing subject ids (as the index) and
+                cognitive reserve data
+    :param columns: List of strings containing column names for columns in data
+                    containing cognitive reserve proxies.
+    :param composites: df returned by create_unique_combinations(data, columns)
+    :return cogResComposites: dataframe with every possible unique combination of 'columns' in 'data'.
+    """
     # SANITY CHECK 1 - all column names in cogRes_composites should be unique
-    comboNames = cogRes_composites.columns.tolist()
+    comboNames = composites.columns.tolist()
     unique_elements_list = list(set(comboNames))
     errors = 0
     if len(comboNames) == len(unique_elements_list):
@@ -63,7 +45,7 @@ def create_cogRes_composites(data, columns):
 
     # SANITY CHECK 2 - number of unique column names should equal number of 
     # possible combinations of proxies without repetition
-    number_proxies = len(data.columns.tolist())
+    number_proxies = len(df.columns.tolist())
     
     # calculate number of combos of n elements taken k in k
     def possibleCombos(n, k):
@@ -79,19 +61,19 @@ def create_cogRes_composites(data, columns):
         print("Ok to proceed")
     else:
         print("STOP - CHECK DATA -  \nNumber of unique columns not equal to the number of unique combinations")
-        s += 1
+        errors += 1
         
     # SANITY CHECK 3 - check three random columns are actually combinations of
     # their respective underlying columns - e.g. check edu_occu is actually 
     # equal to the sum of edu and occu columns from original data 
     
     # get 5 random columns
-    randomCols = cogRes_composites.sample(5, axis=1).columns.tolist()
+    randomCols = composites.sample(5, axis=1).columns.tolist()
     
     for col in randomCols:
         underlyingCols = col.split("_")
-        if cogRes_composites[col].equals(
-                cogRes_composites[underlyingCols].sum(axis=1, skipna=False)):
+        if composites[col].equals(
+                composites[underlyingCols].sum(axis=1, skipna=False)):
             print("Ok to proceed")
         else:
             print("\nSTOP - CHECK DATA \n" + col + 
@@ -99,7 +81,6 @@ def create_cogRes_composites(data, columns):
             errors += 1
     
     if errors == 0:
-        return cogRes_composites
+        print('Composites created fine - no problems identified')
     else:
-        print('No data returned because of error - please inspect data')
-    
+        print('Error in creation of composites - please inspect data\n See above message for further info')
